2c229c8c4111f163025b9cac46a481a7752bbaa3
rei0104 <tuyetngan.080805@gmail.com>
2026-02-13 22:25:21 +0700

    test1

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import sqlite3
+import os
+from flask import Flask, request
 
-def get_user(username):
-    # LỖI BẢO MẬT: SQL Injection (không nên cộng chuỗi trực tiếp)
-    query = "SELECT * FROM users WHERE username = '" + username + "'"
-    print("Đang chạy câu lệnh: " + query)
+app = Flask(__name__)
+
+@app.route('/login')
+def login():
+    username = request.args.get('username')
     
-    # LỖI BẢO MẬT: Lộ mật khẩu (Hardcoded Secret)
+    # LỖI BẢO MẬT 1 (SAST - B608): SQL Injection
+    # Bandit sẽ phát hiện việc cộng chuỗi trực tiếp vào query
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+    
+    # LỖI BẢO MẬT 2 (SAST - B105): Hardcoded Password
+    # Bandit sẽ phát hiện mật khẩu nằm chình ình trong code
     db_password = "admin_password_123" 
     
-    return query
+    return f"Đang kiểm tra user: {username} với query: {query}"
 
 if __name__ == "__main__":
-    get_user("admin")
-# test
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)

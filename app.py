20039a991cd4aab6630b9f841951b84ca1dbc5cc
MaTuyetNganHCMUS <23120302@student.hcmus.edu.vn>
2026-02-13 13:36:48 +0700

    Add add.py
    Added a function to retrieve user data from the database, but includes security vulnerabilities such as SQL injection and hardcoded passwords.

---
+++ app.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def get_user(username):
+    # LỖI BẢO MẬT: SQL Injection (không nên cộng chuỗi trực tiếp)
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+    print("Đang chạy câu lệnh: " + query)
+    
+    # LỖI BẢO MẬT: Lộ mật khẩu (Hardcoded Secret)
+    db_password = "admin_password_123" 
+    
+    return query
+
+if __name__ == "__main__":
+    get_user("admin")
